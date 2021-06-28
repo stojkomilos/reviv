5b8b81189d0912fd5ae53b3043e2d45b72f4dffb
infernotnt <infernotnt@protonmail.com>
2021-06-28 16:33:47 +0200

    Prvi Working build

--- compile.sh
+++ compile.sh
@@ -8,8 +8,13 @@ printf "Project directory: $project_dir\n"
 g++ "$project_dir"/src/main.cpp \
     -I"$project_dir"/src/ \
       "$project_dir"/dependencies/glad.c \
+      "$project_dir"/dependencies/stb_image.cpp \
     -I"$project_dir"/dependencies/include \
     -L"$project_dir"/dependencies/lib \
+      "$project_dir"/src/core/*.cpp \
+      "$project_dir"/src/entity/*.cpp \
+      "$project_dir"/src/physics/*.cpp \
+      "$project_dir"/src/renderer/*.cpp \
     -l:libglfw3.a \
     -lGL \
     -lwayland-client \
