973618f987dc5c3d8470905ab8bf10dc3f6ebd2a
infernotnt <infernotnt@protonmail.com>
2021-06-30 08:39:51 +0200

    c

--- compile.sh
+++ compile.sh
@@ -6,6 +6,21 @@ project_dir=/home/milos/reviv2
 printf "Project directory: $project_dir\n"
 mkdir "$project_dir"/bin
 
+#g++ "$project_dir"/src/main.cpp \
+#    -I"$project_dir"/src/ \
+#      "$project_dir"/dependencies/glad.c \
+#      "$project_dir"/**.cpp
+#      "$project_dir"/dependencies/stb_image.cpp \
+#    -I"$project_dir"/dependencies/include \
+#    -L"$project_dir"/dependencies/lib \
+#    -l:libglfw3.a \
+#    -lGL \
+#    -lwayland-client \
+#    -lpthread -ldl \
+#    -o izlaz
+#    #-o "$project_dir"/izlaz
+
+
 g++ "$project_dir"/src/main.cpp \
     -I"$project_dir"/src/ \
       "$project_dir"/dependencies/glad.c \
@@ -23,7 +38,7 @@ g++ "$project_dir"/src/main.cpp \
     -o izlaz
     #-o "$project_dir"/izlaz
 
-#-lX11 -lXrandr -lXi \
+##-lX11 -lXrandr -lXi \
 
 if [ $? == 0 ]
 then
