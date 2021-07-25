91de714ce59e78575ed2cd47f5a9db3c5952253b
asdf <asdf>
2021-07-24 23:58:08 -0700

    c

--- scripts/Build-Release-Windows.bat
+++ scripts/Build-Release-Windows.bat
@@ -4,5 +4,5 @@ mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Release ../
 cmake --build ./ -j24
-echo ^<ESC^>[32m [32m Building complete. Your executable path is: ../build/bin/Release-Windows/sandbox.exe [0m
+echo [32mBuilding complete. Your executable path is: ../build/bin/Release-Windows/sandbox.exe [0m
 pause
